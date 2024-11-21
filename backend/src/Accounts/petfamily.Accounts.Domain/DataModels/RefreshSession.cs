bd375c9d1dd592578bb35d9fc30d6f15bf29d5cd
andreyberkov666 <andreyberkov666@mail.ru>
2024-11-21 11:21:48 +0300

    dsfsda

---
+++ backend/src/Accounts/petfamily.Accounts.Domain/DataModels/RefreshSession.cs
@@ -0,0 +1,15 @@
+﻿namespace Petfamily.Accounts.Domain.DataModels;
+
+public class RefreshSession 
+{
+    public Guid Id { get; init; }
+    public Guid UserId { get; init; }
+    public User User { get; init; } = null!;
+    public Guid RefreshTokenId { get; init; }
+    public DateTime ExpiresIn { get; init; }
+    public DateTime CreatedAt { get; init; }
+   
+    private RefreshSession()
+    {
+    }
+}
